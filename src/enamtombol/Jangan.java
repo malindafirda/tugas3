e5e1865d91128403e2abb8e8bc4fa265e9cd52e8
malindafirda <malindafirda@yahoo.com>
2015-10-11 02:05:06 +0700

    tugas3

---
+++ src/enamtombol/Jangan.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package enamtombol;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author uuser
+ */
+public class Jangan {
+void tolongJangan(){
+    JOptionPane.showMessageDialog(null, "Dibilang jangan malah ngeyel");
+}
+    
+}    
+
---
+++ src/enamtombol/Pilih.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package enamtombol;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author uuser
+ */
+public class Pilih {
+        void janganPilih(){
+            JOptionPane.showMessageDialog(null, "MAAF, COBA LAGI");
+}
+}
